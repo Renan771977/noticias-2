7352ac98aee414a3a5acbc109ab2c3dace62b34c
Renan Alves Paulino <980276@senacminas.edu.br>
2026-06-24 15:13:26 -0300

    server back alterado

--- backend/js/server.js
+++ backend/js/server.js
@@ -1,10 +1,31 @@
 import express from 'express';
-const dirname = import.meta.dirname;
+import cors from 'cors';
+import db from './db.js'; 
 
 const app = express();
-app.use(express.static(dirname));
 
-const PORT = process.env.PORT || 3001;
+app.use(cors()); 
+app.use(express.json()); 
+
+app.post('/noticias', async (req, res) => {
+    try {
+        const { titulo, conteudo, imagem, link } = req.body;
+
+        const sql = 'INSERT INTO noticias (titulo, conteudo, imagem, link) VALUES (?, ?, ?, ?)';
+        
+        const resultado = await db.executarQuery(sql, [titulo, conteudo, imagem, link]);
+
+        const idInserido = resultado[0].insertId;
+
+        res.status(201).json({ insertId: idInserido });
+
+    } catch (erro) {
+        console.error("Erro na rota /noticias:", erro);
+        res.status(500).json({ erro: "Erro ao salvar no banco de dados." });
+    }
+});
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
